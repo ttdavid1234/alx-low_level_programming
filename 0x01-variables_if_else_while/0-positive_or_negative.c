3a2b99d2b5578132a8bd9dbc898708db52e0b64c
Tobenna Emmanuel Nwosu <tobennanwosu1@gmail.com>
2022-09-09 16:36:49 -0700

    mine

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,24 +4,16 @@
 
 /* more headers goes there */
 
+/* betty style doc for function main goes there *
+ *int main(void)
+ {
 
 
-/* betty style doc for function main goes there */
+int n;
 
-int main(void)
-
-{
-
-		int n;
-
-
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-					/* your code goes there */
-
-					return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+return (0);
 
 }
