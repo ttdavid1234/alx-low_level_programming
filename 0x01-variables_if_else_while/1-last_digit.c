7a6dbd920375ee69a3f30d58a5a498b1e890d11f
Tobenna Emmanuel Nwosu <tobennanwosu1@gmail.com>
2022-09-09 17:19:08 -0700

    mine

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,15 +5,10 @@
 #include <stdio.h>
 
 /**
+ * main - Prints a text according number
  *
- *  * main - Prints a text according number
- *
- *   *
- *
- *    * Return: Always (Success)
- *
- *     */
-
+ * Return: Always (Success)
+ */
 int main(void)
 {
 	int n, lastd;
